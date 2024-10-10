9c1109d942b3eb262adbd416083c3e1178d972bc
sabrinaz35 <sj.zuurbier@gmail.com>
2024-10-10 10:53:00 +0200

    //

--- code.php
+++ code.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $message = $_POST["message"];
+
+        $to = "sj.zuurbier@ziggo.nl";
+        $subject = "User  Request";
+        $body = "Name: $name\nEmail: $email\nMessage: $message";
+
+        mail($to, $subject, $body);
+        echo "Email sent successfully!";
+    }
+?>
