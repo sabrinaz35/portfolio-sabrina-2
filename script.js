e3c16b92246e151c788f4787442a6979a944ce27
sabrinaz35 <sj.zuurbier@gmail.com>
2024-11-07 11:45:04 +0100

    slideshow gefixt

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 console.log("java is goed geconnect");
 
-//Regelen van hoe de hond eruit ziet, dmv. array
-//let plaatjeHondArray = ["hond1.jpeg", "hond-boos.jpeg", "hond-tong.jpeg", "hond-verbaasd.jpeg", "hond-verdrietig.jpeg"]; //de plaatjes neerzetten in de javascript
+let slideIndex = 0;
+showSlides();
 
-//code wat in een if eles ofzo gezet moet worden als plaatjes moeten wisselen
-//document.querySelector(".hond").src= 'images/'+plaatjeHondArray[4];
-
-//dit is de code die ik html moet zetten onderaan, dus onder de footer, maar nog wel in de body
-//<script src="script.js"></script>
-
-let pollutionpartyArray = ["pollutionparty.png", "hond-boos.jpeg", "hond-tong.jpeg", "hond-verbaasd.jpeg", "hond-verdrietig.jpeg"];
-
-let timer = setInterval (veranderPlaatje, 500)
-
-function veranderPlaatje () {
-    let i;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-      }
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); 
 }
 
+//er moet nog iets veranderd worden want die code werkt nog niet helemaal, hij gaat namelijk ineens heel snel.
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
